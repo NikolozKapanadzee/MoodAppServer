9e152892d7814aefe5d63b37d8a41bcdfa9448e5
NikolozKapanadzee <nkapanadze369@gmail.com>
2025-06-29 12:40:20 +0400

    Fix: Build Errors

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
 async function bootstrap() {
   console.log('Starting NestJS app...');
   const app = await NestFactory.create(AppModule);
@@ -12,9 +11,12 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://your-frontend-domain.com'],
+    credentials: true,
+  });
   const port = process.env.PORT ?? 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`App listening on port ${port}`);
 }
 bootstrap();
